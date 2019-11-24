f426692f67fd39557abda0dbf9bf9055039092b5
Henrique Negromonte <henrique.negromonte@hotmail.com>
2019-11-24 15:58:09 -0300

    Atualização, script em questão cria e insere valores padrão

--- Documentos/Cria Banco.sql
+++ Documentos/Cria Banco.sql
@@ -203,6 +203,66 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Frase` (
   PRIMARY KEY (`Id_frase`))
 ENGINE = InnoDB;
 
+/* Script Inserts ---------------------------------------------------------------------------------------------------------------- 
+	Inserção de dados nas tabelas. e.g Cadastro de Usuarios, receitas, despesas, metas etc.
+*/
+
+-- Frase -------------------------------
+USE mydb;
+INSERT INTO Frase (id_frase, frase, autor) VALUES
+	(1,"Seu dinheiro cresce na medida em que você cresce", "T. Harv Eker"),
+	(2,"Só o tolo confunde valor com preço", "Antônio Machado"),
+	(3,"Nunca gaste seu dinheiro antes de recebê-lo", "Thomas Jefferson"),
+	(4,"Se uma pessoa adquire a atitude correta em relação 
+		ao dinheiro, isso ajudará a endireitar quase todas as
+		outras áreas de sua vida", "Billy Graham"),
+    (5,"Economia, frequentemente, não tem relação com o total de 
+		dinheiro gasto, mas com a sabedoria empregada ao gastá-lo", "Henry Ford"),
+	(6,"Devagar! Quem mais corre tropeça!", "William Shakespeare"),
+	(7,"Dinheiro é o oposto do clima. Niguém fala sobre ele, 
+		mas todo mundo faz algo a respeito", "Rebecca Johnson"),
+    (8,"Cuidado com as pequenas despesas; um pequeno vazamento 
+		afundará um grande navio.", "Benjamin Franklin"),
+	(9,"Se você almeja ser rico, pense em poupar assim como você 
+		pensa em ganhar", "Benjamin Franklin"),
+	(10,"Dinheiro é o oposto do clima. Ninguém fala sobre ele, 
+		mas todo mundo faz algo a respeito", "Warren Buffett");
+        
+-- Tipo_despesa -------------------------------
+INSERT INTO Tipo_despesa (id_tipo_despesa, nome) VALUES
+	(1,"Fixa"),
+	(2,"Variável"),
+	(3,"Temporária");
+
+-- Tipo_receita -------------------------------
+INSERT INTO Tipo_receita (id_tipo_receita, nome) VALUES
+	(1,"Fixa"),
+	(2,"Variável");
+    
+    
+-- Moeda ------------------------------- Valores com base na cotação do dia 10/11
+INSERT INTO Moeda (id_moeda, nome, moeda_para_RS, RS_para_moeda) VALUES
+	(1,"Real", 1.00, 1.00);
+    
+
+-- Usuario ---------------------------------------
+INSERT INTO Usuario (id_usuario, nome, email, senha, data_nascimento, cidade, estado, pais, moeda_id) VALUES
+	(1, "adm", "adm@gmail.com", "adm", "2000-11-17", 	NULL, NULL, NULL, 1);  
+
+-- Categoria_despesa -------------------------------
+INSERT INTO Categoria_despesa (id_categoria_despesa, nome, descricao, usuario_id) VALUES
+(1,"Alimentação", "Refeiçoes e lanches", 1),
+(2,"Moradia", "Manutenção de equipamentos do domicílio", 1),
+(3,"Educação", "Gastos com livros, eventos e palestras", 2),
+(4,"Lazer", "Passeios, Viagens", 3),
+(5,"Saúde", "Plano de saúde e consultas extras", 2),
+(6,"Transporte", "", 3),
+(7, "Comunicação ", "Celulares, Telefonia", 3);
+    
+-- Receita ---------------------------------------
+INSERT INTO Receita (id_receita, data_receita, descricao, valor, intervalo, usuario_id, tipo_receita_id, moeda_id) VALUES
+	(1, "2016-11-01", "Salário", 5500.00, "Mensal", 1, 1, 1);
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
